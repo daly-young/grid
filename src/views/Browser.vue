bb0b4ff7a78c8806ca4df7d4b3abccbf65348a97
yangjun <yangjun@aicaigroup.com>
2019-03-28 20:13:31 +0800

    test(grid): 静态资源

--- src/views/Browser.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="about">
-    <p>emmm,兼容性不是很友好~</p>
-    <img src="https://user-gold-cdn.xitu.io/2019/3/28/169c36615d1bf507?w=1475&h=328&f=png&s=219514" alt="grid兼容性" width="1200">
-    <p>相关属性的可以到caniuse上具体查询</p>
-    <a href="https://www.caniuse.com/#search=grid">grid相关属性兼容性</a>
-  </div>
-</template>
--- src/views/Example9.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="about">
-        <h3>grid-template属性是grid-template-columns、grid-template-rows和grid-template-areas这三个属性的合并简写形式。</h3> 
-        <h3>grid属性是grid-template-rows、grid-template-columns、grid-template-areas、 grid-auto-rows、grid-auto-columns、grid-auto-flow这六个属性的合并简写形式。</h3>
-        <p>写起来比较魔性，不介意大家平时书写，具体可以去<a href="https://www.zhangxinxu.com/wordpress/2018/11/display-grid-css-css3/">张鑫旭的博客</a>详细解读一下</p>
-  </div>
-</template>
-
