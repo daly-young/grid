3e9d308383cd80683f0639957ac2557bade1874a
yangjun <yangjun@aicaigroup.com>
2019-03-28 20:52:19 +0800

    fix(grid): 模式

--- src/router.js
+++ src/router.js
@@ -5,7 +5,7 @@ import Home from './views/Home.vue'
 Vue.use(Router)
 
 export default new Router({
-    mode: 'history',
+    mode: 'hash',
     base: process.env.BASE_URL,
     routes: [
         {
@@ -16,77 +16,77 @@ export default new Router({
         {
             path: '/about',
             name: 'about',
-            component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+            component: () => import('./views/About.vue')
         },
         {
             path: '/browser',
             name: 'browser',
-            component: () => import(/* webpackChunkName: "about" */ './views/Browser.vue')
+            component: () => import('./views/Browser.vue')
         },
         {
             path: '/example1',
             name: 'example1',
-            component: () => import(/* webpackChunkName: "about" */ './views/Example1.vue')
+            component: () => import('./views/Example1.vue')
         },
         {
             path: '/example2',
             name: 'example2',
-            component: () => import(/* webpackChunkName: "about" */ './views/Example2.vue')
+            component: () => import('./views/Example2.vue')
         },
         {
             path: '/example3',
             name: 'example3',
-            component: () => import(/* webpackChunkName: "about" */ './views/Example3.vue')
+            component: () => import('./views/Example3.vue')
         },
         {
             path: '/example4',
             name: 'example4',
-            component: () => import(/* webpackChunkName: "about" */ './views/Example4.vue')
+            component: () => import('./views/Example4.vue')
         },
         {
             path: '/example5',
             name: 'example5',
-            component: () => import(/* webpackChunkName: "about" */ './views/Example5.vue')
+            component: () => import('./views/Example5.vue')
         },
         {
             path: '/example6',
             name: 'example6',
-            component: () => import(/* webpackChunkName: "about" */ './views/Example6.vue')
+            component: () => import('./views/Example6.vue')
         },
         {
             path: '/example7',
             name: 'example7',
-            component: () => import(/* webpackChunkName: "about" */ './views/Example7.vue')
+            component: () => import('./views/Example7.vue')
         },
         {
             path: '/example8',
             name: 'example8',
-            component: () => import(/* webpackChunkName: "about" */ './views/Example8.vue')
+            component: () => import('./views/Example8.vue')
         },
         {
             path: '/example9',
             name: 'example9',
-            component: () => import(/* webpackChunkName: "about" */ './views/Example9.vue')
+            component: () => import('./views/Example9.vue')
         },
         {
             path: '/example10',
             name: 'example10',
-            component: () => import(/* webpackChunkName: "about" */ './views/Example10.vue')
+            component: () => import('./views/Example10.vue')
         },
         {
             path: '/example11',
             name: 'example11',
-            component: () => import(/* webpackChunkName: "about" */ './views/Example11.vue')
+            component: () => import('./views/Example11.vue')
         },
         {
             path: '/example12',
             name: 'example12',
-            component: () => import(/* webpackChunkName: "about" */ './views/Example12.vue')
+            component: () => import('./views/Example12.vue')
         },
         {
             path: '/example13',
             name: 'example13',
-            component: () => import(/* webpackChunkName: "about" */ './views/Example13.vue')
+            component: () => import('./views/Example13.vue')
         }
     ]
 })
